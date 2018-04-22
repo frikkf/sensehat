d2eb079b6ca977b7d246a84bafeb88a31a8d35c9
Frikk <frikk.fossan@acando.no>
2018-04-22 17:32:24 +0200

    copy from working azure project

---
+++ BME280SensorSimulator.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+import random
+
+from sense_hat import SenseHat
+
+
+class BME280SensorSimulator:
+    def __init__(self):
+        pass
+
+    def read_temperature(self):
+        sense = SenseHat()
+        temp = sense.get_temperature()
+        return temp
+
+    
+    def read_humidity(self):
+        sense = SenseHat()
+        humidity = sense.get_humidity()
+        return humidity
+
+    def read_pressure(self):
+        sense = SenseHat()
+        pressure = sense.get_pressure()
+        return pressure
+
+    
--- app.py
+++ app.py
@@ -1,29 +1,20 @@
-from sense_hat import SenseHat
+#!/usr/bin/env python
+
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
 import random
 import time
 import sys
-import iothub_client
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
-from iothub_client import IoTHubClientRetryPolicy, GetRetryPolicyReturnValue
-from iothub_client_args import get_iothub_opt, OptionError
-
-
-sense = SenseHat()
-
-humidity = sense.get_humidity()
-temp = sense.get_temperature()
-pressure = sense.get_pressure()
-
-
-
-
-print("Humidity"+str(humidity))
-print("Temp"+str(temp))
-print("Pressure"+str(pressure))
-
-sense.show_message(str(temp))
-
+import config as config
+from BME280SensorSimulator import BME280SensorSimulator
+import RPi.GPIO as GPIO
+from Adafruit_BME280 import *
+import re
+from telemetry import Telemetry
 
 # HTTP options
 # Because it can poll "after 9 seconds" polls will happen effectively
@@ -40,49 +31,60 @@ MINIMUM_POLLING_TIME = 9
 MESSAGE_TIMEOUT = 10000
 
 RECEIVE_CONTEXT = 0
-AVG_WIND_SPEED = 10.0
-MIN_TEMPERATURE = 20.0
-MIN_HUMIDITY = 60.0
-MESSAGE_COUNT = 5
-RECEIVED_COUNT = 0
-CONNECTION_STATUS_CONTEXT = 0
+MESSAGE_COUNT = 0
+MESSAGE_SWITCH = True
 TWIN_CONTEXT = 0
 SEND_REPORTED_STATE_CONTEXT = 0
 METHOD_CONTEXT = 0
+TEMPERATURE_ALERT = 30.0
 
 # global counters
 RECEIVE_CALLBACKS = 0
 SEND_CALLBACKS = 0
 BLOB_CALLBACKS = 0
-CONNECTION_STATUS_CALLBACKS = 0
 TWIN_CALLBACKS = 0
 SEND_REPORTED_STATE_CALLBACKS = 0
 METHOD_CALLBACKS = 0
+EVENT_SUCCESS = "success"
+EVENT_FAILED = "failed"
 
-# chose HTTP, AMQP, AMQP_WS or MQTT as transport protocol
+# chose HTTP, AMQP or MQTT as transport protocol
 PROTOCOL = IoTHubTransportProvider.MQTT
 
 # String containing Hostname, Device Id & Device Key in the format:
 # "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-CONNECTION_STRING = "HostName=frikksiothub.azure-devices.net;DeviceId=frikk-rbpi;SharedAccessKey=WG/0OY0mGErtrFSiYTj/c4KcMVfqxFErxB72F+OcXqU="
+telemetry = Telemetry()
 
-MSG_TXT = "{\"deviceId\": \"myPythonDevice\",\"windSpeed\": %.2f,\"temperature\": %.2f,\"humidity\": %.2f}"
+if len(sys.argv) < 2:
+    print ( "You need to provide the device connection string as command line arguments." )
+    telemetry.send_telemetry_data(None, EVENT_FAILED, "Device connection string is not provided")
+    sys.exit(0)
 
-# some embedded platforms need certificate information
-def set_certificates(client):
-    from iothub_client_cert import CERTIFICATES
-    try:
-        client.set_option("TrustedCerts", CERTIFICATES)
-        print ( "set_option TrustedCerts successful" )
-    except IoTHubClientError as iothub_client_error:
-        print ( "set_option TrustedCerts failed (%s)" % iothub_client_error )
+def is_correct_connection_string():
+    m = re.search("HostName=.*;DeviceId=.*;", CONNECTION_STRING)
+    if m:
+        return True
+    else:
+        return False
+
+CONNECTION_STRING = sys.argv[1]
+
+if not is_correct_connection_string():
+    print ( "Device connection string is not correct." )
+    telemetry.send_telemetry_data(None, EVENT_FAILED, "Device connection string is not correct.")
+    sys.exit(0)
+
+MSG_TXT = "{\"deviceId\": \"Raspberry Pi - Python\",\"temperature\": %f,\"humidity\": %f}"
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(config.GPIO_PIN_ADDRESS, GPIO.OUT)
 
 def receive_message_callback(message, counter):
     global RECEIVE_CALLBACKS
     message_buffer = message.get_bytearray()
     size = len(message_buffer)
     print ( "Received Message [%d]:" % counter )
-    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode('utf-8'), size) )
+    print ( "    Data: <<<%s>>> & Size=%d" % (message_buffer[:size].decode("utf-8"), size) )
     map_properties = message.properties()
     key_value_pair = map_properties.get_internals()
     print ( "    Properties: %s" % key_value_pair )
@@ -102,57 +104,60 @@ def send_confirmation_callback(message, result, user_context):
     print ( "    Properties: %s" % key_value_pair )
     SEND_CALLBACKS += 1
     print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
-
-
-def connection_status_callback(result, reason, user_context):
-    global CONNECTION_STATUS_CALLBACKS
-    print ( "Connection status changed[%d] with:" % (user_context) )
-    print ( "    reason: %d" % reason )
-    print ( "    result: %s" % result )
-    CONNECTION_STATUS_CALLBACKS += 1
-    print ( "    Total calls confirmed: %d" % CONNECTION_STATUS_CALLBACKS )
+    led_blink()
 
 
 def device_twin_callback(update_state, payload, user_context):
     global TWIN_CALLBACKS
-    print ( "")
-    print ( "Twin callback called with:")
-    print ( "updateStatus: %s" % update_state )
-    print ( "context: %s" % user_context )
-    print ( "payload: %s" % payload )
+    print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
     TWIN_CALLBACKS += 1
     print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
 
 
 def send_reported_state_callback(status_code, user_context):
     global SEND_REPORTED_STATE_CALLBACKS
-    print ( "Confirmation[%d] for reported state received with:" % (user_context) )
-    print ( "    status_code: %d" % status_code )
+    print ( "Confirmation for reported state received with:\nstatus_code = [%d]\ncontext = %s" % (status_code, user_context) )
     SEND_REPORTED_STATE_CALLBACKS += 1
     print ( "    Total calls confirmed: %d" % SEND_REPORTED_STATE_CALLBACKS )
 
 
 def device_method_callback(method_name, payload, user_context):
-    global METHOD_CALLBACKS
+    global METHOD_CALLBACKS,MESSAGE_SWITCH
     print ( "\nMethod callback called with:\nmethodName = %s\npayload = %s\ncontext = %s" % (method_name, payload, user_context) )
     METHOD_CALLBACKS += 1
     print ( "Total calls confirmed: %d\n" % METHOD_CALLBACKS )
     device_method_return_value = DeviceMethodReturnValue()
     device_method_return_value.response = "{ \"Response\": \"This is the response from the device\" }"
     device_method_return_value.status = 200
+    if method_name == "start":
+        MESSAGE_SWITCH = True
+        print ( "Start sending message\n" )
+        device_method_return_value.response = "{ \"Response\": \"Successfully started\" }"
+        return device_method_return_value
+    if method_name == "stop":
+        MESSAGE_SWITCH = False
+        print ( "Stop sending message\n" )
+        device_method_return_value.response = "{ \"Response\": \"Successfully stopped\" }"
+        return device_method_return_value
     return device_method_return_value
+
+
+def blob_upload_conf_callback(result, user_context):
+    global BLOB_CALLBACKS
+    print ( "Blob upload confirmation[%d] received for message with result = %s" % (user_context, result) )
+    BLOB_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % BLOB_CALLBACKS )
 
 
 def iothub_client_init():
     # prepare iothub client
     client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    client.set_option("product_info", "HappyPath_RaspberryPi-Python")
     if client.protocol == IoTHubTransportProvider.HTTP:
         client.set_option("timeout", TIMEOUT)
         client.set_option("MinimumPollingTime", MINIMUM_POLLING_TIME)
     # set the time until a message times out
     client.set_option("messageTimeout", MESSAGE_TIMEOUT)
-    # some embedded platforms need certificate information
-    set_certificates(client)
     # to enable MQTT logging set to 1
     if client.protocol == IoTHubTransportProvider.MQTT:
         client.set_option("logtrace", 0)
@@ -163,19 +168,6 @@ def iothub_client_init():
             device_twin_callback, TWIN_CONTEXT)
         client.set_device_method_callback(
             device_method_callback, METHOD_CONTEXT)
-    if client.protocol == IoTHubTransportProvider.AMQP or client.protocol == IoTHubTransportProvider.AMQP_WS:
-        client.set_connection_status_callback(
-            connection_status_callback, CONNECTION_STATUS_CONTEXT)
-
-    retryPolicy = IoTHubClientRetryPolicy.RETRY_INTERVAL
-    retryInterval = 100
-    client.set_retry_policy(retryPolicy, retryInterval)
-    print ( "SetRetryPolicy to: retryPolicy = %d" %  retryPolicy)
-    print ( "SetRetryPolicy to: retryTimeoutLimitInSeconds = %d" %  retryInterval)
-    retryPolicyReturn = client.get_retry_policy()
-    print ( "GetRetryPolicy returned: retryPolicy = %d" %  retryPolicyReturn.retryPolicy)
-    print ( "GetRetryPolicy returned: retryTimeoutLimitInSeconds = %d" %  retryPolicyReturn.retryTimeoutLimitInSeconds)
-
     return client
 
 
@@ -192,9 +184,7 @@ def print_last_message_time(client):
 
 
 def iothub_client_sample_run():
-
     try:
-
         client = iothub_client_init()
 
         if client.protocol == IoTHubTransportProvider.MQTT:
@@ -202,70 +192,64 @@ def iothub_client_sample_run():
             reported_state = "{\"newState\":\"standBy\"}"
             client.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, SEND_REPORTED_STATE_CONTEXT)
 
-        while True:
-            # send a few messages every minute
-            print ( "IoTHubClient sending %d messages" % MESSAGE_COUNT )
+        if not config.SIMULATED_DATA:
+            sensor = BME280(address = config.I2C_ADDRESS)
+        else:
+            sensor = BME280SensorSimulator()
 
-            for message_counter in range(0, MESSAGE_COUNT):
-                temperature = MIN_TEMPERATURE + (random.random() * 10)
-                humidity = MIN_HUMIDITY + (random.random() * 20)
+        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_SUCCESS, "IoT hub connection is established")
+        while True:
+            global MESSAGE_COUNT,MESSAGE_SWITCH
+            if MESSAGE_SWITCH:
+                # send a few messages every minute
+                print ( "IoTHubClient sending %d messages" % MESSAGE_COUNT )
+                temperature = sensor.read_temperature()
+                humidity = sensor.read_humidity()
                 msg_txt_formatted = MSG_TXT % (
-                    AVG_WIND_SPEED + (random.random() * 4 + 2),
                     temperature,
                     humidity)
-                # messages can be encoded as string or bytearray
-                if (message_counter & 1) == 1:
-                    message = IoTHubMessage(bytearray(msg_txt_formatted, 'utf8'))
-                else:
-                    message = IoTHubMessage(msg_txt_formatted)
+                print (msg_txt_formatted)
+                message = IoTHubMessage(msg_txt_formatted)
                 # optional: assign ids
-                message.message_id = "message_%d" % message_counter
-                message.correlation_id = "correlation_%d" % message_counter
-                # optional: assign propertiesp
+                message.message_id = "message_%d" % MESSAGE_COUNT
+                message.correlation_id = "correlation_%d" % MESSAGE_COUNT
+                # optional: assign properties
                 prop_map = message.properties()
-                prop_map.add("temperatureAlert", 'true' if temperature > 28 else 'false')
+                prop_map.add("temperatureAlert", "true" if temperature > TEMPERATURE_ALERT else "false")
 
-                client.send_event_async(message, send_confirmation_callback, message_counter)
-                print ( "IoTHubClient.send_event_async accepted message [%d] for transmission to IoT Hub." % message_counter )
+                client.send_event_async(message, send_confirmation_callback, MESSAGE_COUNT)
+                print ( "IoTHubClient.send_event_async accepted message [%d] for transmission to IoT Hub." % MESSAGE_COUNT )
 
-            # Wait for Commands or exit
-            print ( "IoTHubClient waiting for commands, press Ctrl-C to exit" )
-
-            status_counter = 0
-            while status_counter <= MESSAGE_COUNT:
                 status = client.get_send_status()
                 print ( "Send status: %s" % status )
-                time.sleep(10)
-                status_counter += 1
+                MESSAGE_COUNT += 1
+            time.sleep(config.MESSAGE_TIMESPAN / 1000.0)
 
     except IoTHubError as iothub_error:
         print ( "Unexpected error %s from IoTHub" % iothub_error )
+        telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_FAILED, "Unexpected error %s from IoTHub" % iothub_error)
         return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 
     print_last_message_time(client)
 
+def led_blink():
+    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.HIGH)
+    time.sleep(config.BLINK_TIMESPAN / 1000.0)
+    GPIO.output(config.GPIO_PIN_ADDRESS, GPIO.LOW)
 
 def usage():
     print ( "Usage: iothub_client_sample.py -p <protocol> -c <connectionstring>" )
     print ( "    protocol        : <amqp, amqp_ws, http, mqtt, mqtt_ws>" )
     print ( "    connectionstring: <HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>>" )
 
+def parse_iot_hub_name():
+    m = re.search("HostName=(.*?)\.", CONNECTION_STRING)
+    return m.group(1)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print ( "\nPython %s" % sys.version )
     print ( "IoT Hub Client for Python" )
-
-    try:
-        (CONNECTION_STRING, PROTOCOL) = get_iothub_opt(sys.argv[1:], CONNECTION_STRING, PROTOCOL)
-    except OptionError as option_error:
-        print ( option_error )
-        usage()
-        sys.exit(1)
-
-    print ( "Starting the IoT Hub Python sample..." )
-    print ( "    Protocol %s" % PROTOCOL )
-    print ( "    Connection string=%s" % CONNECTION_STRING )
 
     iothub_client_sample_run()
