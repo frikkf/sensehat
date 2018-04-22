3018d8ee90ab66c2324c5116784c279e263b732e
Frikk <frikk.fossan@acando.no>
2018-04-22 15:00:45 +0200

    show temp

--- sensehat.py
+++ sensehat.py
@@ -5,11 +5,12 @@ sense = SenseHat()
 humidity = sense.get_humidity()
 temp = sense.get_temperature()
 pressure = sense.get_pressure()
-sense.show_message("Hello world!")
+
+
 
 
 print("Humidity"+str(humidity))
 print("Temp"+str(temp))
 print("Pressure"+str(pressure))
 
-
+sense.show_message(temp)
