da7c9bd5423d6f10be6ccfb056aca9cb4a4a3d1e
Frikk <frikk.fossan@acando.no>
2018-04-22 14:59:03 +0200

    sensors

--- sensehat.py
+++ sensehat.py
@@ -2,4 +2,14 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
+humidity = sense.get_humidity()
+temp = sense.get_temperature()
+pressure = sense.get_pressure()
 sense.show_message("Hello world!")
+
+
+print("Humidity"+str(humidity))
+print("Temp"+str(temp))
+print("Pressure"+str(pressure))
+
+
